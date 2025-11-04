793022c50b55c9ce49e80b85ae8d9edbe27d705b
unknown <kamiri.lilia1952006@gmail.com>
2025-11-04 01:18:29 +0100

    ongoing

---
+++ exo1/script.js
@@ -0,0 +1,29 @@
+"use strict"; // Slide 51 : mode strict pour éviter les erreurs globales
+
+// Chaque fonction illustre une notion de base du langage JavaScript
+// et affiche son résultat dans la console (Slide 33)
+
+function f1() {
+  console.log("John");
+}
+
+function f2() {
+ console.log("Doe");
+  
+}
+
+function f3() {
+  console.log("John-Doe");
+}
+
+function f4() {
+  console.log(30);
+}
+
+function f5() {
+  console.log(15);
+}
+
+function f6() {
+  console.log(3.14);
+}
